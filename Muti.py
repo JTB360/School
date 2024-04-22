bc746cc4eafa47abbe71fb3618b456b2dfc28e86
JTB360 <167726011+JTB360@users.noreply.github.com>
2024-04-21 19:59:14 -0600

    updates

---
+++ Muti.py
@@ -0,0 +1,14 @@
+#Part 2
+
+
+#Takes two inputs from the user
+num1_input = input('Please enter a number:')
+num2_input = input('Please enter a second number:')
+
+#Converts user inputs to integers, multiplies and divides these integers
+num_mult = int(num1_input) * int(num2_input)
+num_divide = int(num1_input) / int(num2_input)
+
+#Prints the multiplied/divided integers
+print (num_mult)
+print (num_divide)
