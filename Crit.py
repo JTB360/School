0d817fb4da65612d821a6d34ddfa10c54915f1c8
JTB360 <167726011+JTB360@users.noreply.github.com>
2024-04-21 19:43:37 -0600

    Part 1

---
+++ Crit.py
@@ -0,0 +1,14 @@
+#Part 1
+
+ 
+#Takes input from the user
+num1_input = input('Please enter a number:')
+num2_input = input('Please enter a second number:')
+
+#Converts user inputs to integers, adds or subtracts these integers
+num_add = int(num1_input) + int(num2_input)
+num_sub = int(num1_input) - int(num2_input)
+
+#Prints the added/subtracted integers
+print (num_add)
+print (num_sub)
